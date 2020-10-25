319133c127691462d799f055068b23862bdd057d
Tucker Leach <leachtucker@gmail.com>
2020-10-25 16:13:24 -0700

    Imported libraries. Setting up Redux flow

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore();
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
